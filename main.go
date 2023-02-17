f2f782355f58e27eda7f8a90cf178a430998e25f
unknown <narkorn2015@gmail.com>
2023-02-17 12:35:59 +0700

    Routing

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("hello server")
+
+	r := gin.Default()
+	r.GET("/hello", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"greeting": "Hello Server Default"})
+	})
+
+	r.Run()
 }
